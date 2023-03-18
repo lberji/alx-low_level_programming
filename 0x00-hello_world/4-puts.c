f25b945476b285bd386d60d36be9c4fef6efe212
lberji <lberjiyoussef@gmail.com>
2023-03-18 14:17:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
 #!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char str[50];
+
+   strcpy(str, "Programming is like building a multilingual puzzle");
+
+   puts(str);
+   
+   return(0);
+}

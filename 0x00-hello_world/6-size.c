7c318ab3974885316273f1cd83d944abcfd07d56
lberji <lberjiyoussef@gmail.com>
2023-03-18 16:10:29 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - printf fonction
+* Return: 0
+*/
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
+printf("Size of an int: 4 byte(s)\n", (unsigned long) sizeof(b));
+printf("Size of a long int: 4 byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a long long int: 8 byte(s)\n", (unsigned long) sizeof(d));
+printf("Size of a float: 4 byte(s)\n", (unsigned long) sizeof(f));
+return (0);
+}

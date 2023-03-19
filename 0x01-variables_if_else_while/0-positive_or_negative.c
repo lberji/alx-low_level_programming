e1a28f66ba53829070c1b72fa526770c2225e110
lberji <lberjiyoussef@gmail.com>
2023-03-19 06:05:51 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,14 @@ int main(void)
     if (n>0)
 {
     printf("%d is positive",n);
-else if (n<0)
+}
+if (n<0)
 {
     printf("%d is negative",n);
-else if (n=0)
+}
+if (n=0)
 {
     printf("%d is zero",n);
-}
-
-}
-
 }
          return (0);
 }

931da41173792b05162c710a6129b743ddd7f2e8
lberji <lberjiyoussef@gmail.com>
2023-03-19 08:11:07 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - Entry point
  *
  * Description: 'the program's description'
- * @parameter: describe the parameter
+ * parameter: describe the parameter
  *
  * Return: Always 0 (Success)
  */
